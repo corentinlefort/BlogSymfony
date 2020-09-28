fe7f36fd3a591dc42ae339dc374aa94547c32c4e
Corentin LEFORT <corentin.lefort1@hotmail.com>
2020-09-28 13:29:07 +0200

    Traduction d'un texte

--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -67,7 +67,7 @@ class LoginFormAuthenticator extends AbstractFormLoginAuthenticator
 
         if (!$user) {
             // fail authentication with a custom error
-            throw new CustomUserMessageAuthenticationException('Username could not be found.');
+            throw new CustomUserMessageAuthenticationException('Aucun utilisateur trouvé pour ce pseudo.');
         }
 
         return $user;
